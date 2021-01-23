2fa02f1fd99766d6fe189b943f0454d946ad3054
= <=>
2021-01-23 19:30:14 -0300

    first concept of react documentation

---
+++ my-app/src/introducing-jsx.js
@@ -0,0 +1,17 @@
+const user = {
+  firstName : "Eduardo",
+  lastName  : "Matos"
+}
+function buildName(user) {
+  return `${user.firstName} ${user.lastName}`
+}
+function App() {
+  return (
+    <div>
+      <h1>Hello, {buildName(user)}</h1>
+      <h2>How are you, {user.firstName} ?</h2>
+    </div>
+  );
+}
+
+export default App;
